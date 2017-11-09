d1205b836b005c2eb7b3faeaa3aae8b113d3d75d
Taras <khmelovski@gmail.com>
2017-11-09 22:21:35 +0200

    add bcryptjs and register user beck end validations

--- routes/users.js
+++ routes/users.js
@@ -14,6 +14,28 @@ router.get('/register', (req, res) => {
   res.render('users/register');
 });
 
+// Register Form POST
+router.post('/register', (req, res) => {
+  let errors = [];
+
+  if (req.body.password != req.body.password2) {
+    errors.push({ text: 'Password do not match' });
+  }
+  if (req.body.password.length < 4) {
+    errors.push({ text: 'Password mast be at least 4 characters' });
+  }
+  if (errors.length > 0) {
+    res.render('users/register', {
+      errors: errors,
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      password2: req.body.password2
+    });
+  } else {
+    res.send('past');
+  }
+});
 
 
 module.exports = router;
