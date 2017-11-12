2ad466084e7198338d1a6b1e4cf0ef01ec8463ba
Taras <khmelovski@gmail.com>
2017-11-12 23:09:55 +0200

    update mLab info

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = {
-    mongoURI: 'mongodb://<dbuser>:<dbpassword>@ds159845.mlab.com:59845/vidjot'
+    mongoURI: 'mongodb://brad:brad@ds159845.mlab.com:59845/vidjot'
   };
 } else {
   module.exports = {
