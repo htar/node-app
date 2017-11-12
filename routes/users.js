6a3550bd2e900b1c80d9fda6f5a1a9f827eb6419
Taras <khmelovski@gmail.com>
2017-11-12 09:45:23 +0200

    add  log out with passport

--- routes/users.js
+++ routes/users.js
@@ -78,5 +78,13 @@ router.post('/register', (req, res) => {
   }
 });
 
+// Logout user
+
+router.get('/logout', (req,res) => { 
+  req.logout();
+  req.flash('success_msg', 'You are logged out');
+  res.redirect('/users/login');
+});
+
 
 module.exports = router;
